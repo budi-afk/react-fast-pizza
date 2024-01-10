934a31b747cd76a7d49029641b324aae91d43a6a
budi-afk <budiman1347@gmail.com>
2024-01-10 12:14:50 +0700

    setup project & installing react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Cart from "./features/cart/Cart";
+import Menu from "./features/menu/Menu";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+    ],
+  },
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;

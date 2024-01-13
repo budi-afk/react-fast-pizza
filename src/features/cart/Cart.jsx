e79a3fbfe248286717faf9a50e233a974ff37615
budi-afk <budiman1347@gmail.com>
2024-01-13 21:00:19 +0700

    styling the app

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,44 +1,58 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import Button from '../../ui/Button'
+import LinkButton from '../../ui/LinkButton'
+import CartItem from './CartItem'
 
 const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+    {
+        pizzaId: 12,
+        name: 'Mediterranean',
+        quantity: 2,
+        unitPrice: 16,
+        totalPrice: 32,
+    },
+    {
+        pizzaId: 6,
+        name: 'Vegetale',
+        quantity: 1,
+        unitPrice: 13,
+        totalPrice: 13,
+    },
+    {
+        pizzaId: 11,
+        name: 'Spinach and Mushroom',
+        quantity: 1,
+        unitPrice: 15,
+        totalPrice: 15,
+    },
+]
 
 function Cart() {
-  const cart = fakeCart;
+    const cart = fakeCart
 
-  return (
-    <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+    return (
+        <div className="py3 px-4">
+            <Link
+                to="/menu"
+                className="text-sm text-blue-500 hover:text-blue-900"
+            >
+                &larr; Back to menu
+            </Link>
 
-      <h2>Your cart, %NAME%</h2>
+            <ul className="mt-3 divide-y divide-stone-200">
+                {cart.map((item) => (
+                    <CartItem item={item} key={item.pizzaId} />
+                ))}
+            </ul>
 
-      <div>
-        <Link to="/order/new">Order pizzas</Link>
-        <button>Clear cart</button>
-      </div>
-    </div>
-  );
+            <h2 className="mt-7 text-left font-semibold">Your cart, %NAME%</h2>
+
+            <div className="mt-6 space-x-2">
+                <Button type="primary">Order Pizzas</Button>
+                <Button type="secondary">Clear cart</Button>
+            </div>
+        </div>
+    )
 }
 
-export default Cart;
+export default Cart
